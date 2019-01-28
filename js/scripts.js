fbee6cd0c5f2e4352e37a1fd89b72e7ef3381c6f
Tony Nicola <tdnicola@gmail.com>
2019-01-27 20:52:53 -0700

    Added Pokemon objects and created array to show objects created.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,51 @@
+/*  Creating var inside of array? Seems like a bad idea...
 var repository = [
-  objectOne {
+  var objectOne = {
     name: 'Bulbasaur',
     height: 7,
-    types: "grass"
-  };
-  objectTwo {
+    types: ['grass', 'poison']
+  },
+  var objectTwo = {
     name: 'Charmander',
     height: 6,
-    types: "fire"
-  };
-  objectThree {
-    name: "squirtle",
+    types: ['fire']
+  },
+   var objectThree = {
+    name: 'squirtle',
     height: 5,
-    types: "water"
-  };
+    types: ['water']
+  }
+]
+
+for (var i = 0; i < repository.length; i++) {
+  document.write(repository[i])
+} */
+
+// Another thought process.. creating each variable beforehand and putting them into the Array.
+var objectOne = {
+  name: 'Bulbasaur',
+  height: 7,
+  types: ['grass', 'poison']
+};
+
+var objectTwo = {
+  name: 'Charmander',
+  height: 6,
+  types: ['fire']
+};
+
+var objectThree = {
+  name: 'squirtle',
+  height: 5,
+  types: ['water']
+};
+
+var repository = [
+  objectOne,
+  objectTwo,
+  objectThree
 ];
+
+for (var i = 0; i < repository.length; i++) {
+  document.write(repository[i].name + '(height: ' + repository[i].height + ')' + repository[i].types);
+}
