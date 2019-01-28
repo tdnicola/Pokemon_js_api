38d96b0085eef94d913891fc78b4554104eeebbb
Tony Nicola <tdnicola@gmail.com>
2019-01-27 18:12:57 -0700

    Created array for pokemon API

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-alert('Hello world');
+var repository = [
+  objectOne {
+    name: 'Bulbasaur',
+    height: 7,
+    types: "grass"
+  };
+  objectTwo {
+    name: 'Charmander',
+    height: 6,
+    types: "fire"
+  };
+  objectThree {
+    name: "squirtle",
+    height: 5,
+    types: "water"
+  };
+];
