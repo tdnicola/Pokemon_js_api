7ce031319a7611d4d7c4d3a2eb1e28ce701321fc
Tony Nicola <tdnicola@gmail.com>
2019-01-28 20:47:17 -0700

    Corrections to the array, removed unneccessary variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,23 @@
-/*  Creating var inside of array? Seems like a bad idea...
+
 var repository = [
-  var objectOne = {
+  {
     name: 'Bulbasaur',
     height: 7,
     types: ['grass', 'poison']
   },
-  var objectTwo = {
+  {
     name: 'Charmander',
     height: 6,
     types: ['fire']
   },
-   var objectThree = {
+  {
     name: 'squirtle',
     height: 5,
     types: ['water']
   }
 ]
 
-for (var i = 0; i < repository.length; i++) {
-  document.write(repository[i])
-} */
-
-// Another thought process.. creating each variable beforehand and putting them into the Array.
+/*  Another thought process.. creating each variable beforehand and putting them into the Array.
 var objectOne = {
   name: 'Bulbasaur',
   types: ['grass', ' poison '],
@@ -45,7 +41,7 @@ var repository = [
   objectTwo,
   objectThree
 ];
-
+*/
 for (var i = 0; i < repository.length; i++) {
   document.write(repository[i].name + ' ' +  repository[i].types + '(height: ' + repository[i].height + ')' + ' ' + '<br>');
   if (repository[i].height >= 7) {
