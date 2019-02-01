aa80d84eefbf60cecf03327d7aecf34a043c1975
Tony Nicola <tdnicola@gmail.com>
2019-01-31 20:51:49 -0700

    Refactoring code, included IIFE, repository pattern.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,77 @@
+// Original working method below, objects in an array
+// var repository = [
+//   {
+//     name: 'Bulbasaur ',
+//     height: 7,
+//     types: ['grass', 'poison ']
+//   },
+//    {
+//     name: 'Charmander',
+//     height: 6,
+//     types: ['fire ']
+//   },
+//     {
+//     name: 'Squirtle ',
+//     height: 5,
+//     types: ['water ']
+//   }
+// ]
 
-var repository = [
+// for loop that prints out all objects
+
+// for (var i = 0; i < repository.length; i++) {
+//   document.write(repository[i].name + ' ' +  repository[i].types + '(height: ' + repository[i].height + ')' + ' ' + '<br>');
+//   if (repository[i].height >= 7) {
+//     document.write('- Wow, that\'s big ');
+//   };
+// };
+
+
+// currently writing only the keys in the object below.
+
+// IIFE testing
+var pokemonRepository = (function() {
+  var data = {};
+
+// currently printing out each line, can change each key to be seperate pokemon
+
+var repository =
   {
-    name: 'Bulbasaur',
-    height: 7,
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Charmander',
-    height: 6,
-    types: ['fire']
-  },
-  {
-    name: 'squirtle',
-    height: 5,
-    types: ['water']
-  }
-]
-
-/*  Another thought process.. creating each variable beforehand and putting them into the Array.
-var objectOne = {
-  name: 'Bulbasaur',
-  types: ['grass', ' poison '],
-  height: 7
-};
-
-var objectTwo = {
-  name: 'Charmander',
-  types: ['fire '],
-  height: 6
-};
-
-var objectThree = {
-  name: 'Squirtle',
-  types: ['water '],
-  height: 5
-};
-
-var repository = [
-  objectOne,
-  objectTwo,
-  objectThree
-];
-*/
-for (var i = 0; i < repository.length; i++) {
-  document.write(repository[i].name + ' ' +  repository[i].types + '(height: ' + repository[i].height + ')' + ' ' + '<br>');
-  if (repository[i].height >= 7) {
-    document.write('- Wow, that\'s big <br>');
+    name: ['Bulbasaur',' Charmander',' Squirtle'],
+    height: [7, 6, 5],
+    types: ['grass and poison', ' fire ', ' water ']
   };
-};
+
+//creating printing without using the for loop
+
+  Object.keys(repository).forEach(function (pokemonData) {
+    document.write(pokemonData + ': ' + repository[pokemonData] + '<br>');
+  });
+
+  // end IIFE testing brackets
+
+})();
+
+
+// trying for each function to print off each pokemon, but above is already an array. Duh it's using object.keys
+
+// prints all key names in the object
+
+//  var allProperties = Object.keys(repository);
+//
+// document.write(allProperties);
+
+// trying for each function to print off each pokemon, but above is already an array.
+
+// object.keys(repository).forEach(function(repKeys) {
+//   document.write(repository[repKeys]);
+// });
+
+// print each pokemon with a for loop
+
+
+
+// trying to mess around with colors?
+// if (repository[0, 1, 2].types = 'grass') {
+//   backgroundColor = '#99C262'
+// };
