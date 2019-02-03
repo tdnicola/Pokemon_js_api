5333c67af7e30db7bc84e6f74a797755eb0a9f94
Tony Nicola <tdnicola@gmail.com>
2019-02-03 12:08:24 -0700

    updated console log test and add function. updated old for loop to print pokemon. removed old commented out code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,43 @@
-// Original working method below, objects in an array
-// var repository = [
-//   {
-//     name: 'Bulbasaur ',
-//     height: 7,
-//     types: ['grass', 'poison ']
-//   },
-//    {
-//     name: 'Charmander',
-//     height: 6,
-//     types: ['fire ']
-//   },
-//     {
-//     name: 'Squirtle ',
-//     height: 5,
-//     types: ['water ']
-//   }
-// ]
+var pokemonRepository = (function () {
+  var repository = [
+    {
+      name: 'Bulbasaur ',
+      height: 7,
+      types: 'grass'
+    },
+    {
+      name: 'Charmander ',
+      height: 6,
+      types: 'fire '
+    },
+    {
+      name: 'Squirtle ',
+      height: 5,
+      types: 'water '
+    }
+  ]
 
-// for loop that prints out all objects
+  function add (repositoryO) {
+    repository.push(repositoryO);
+  }
 
-// for (var i = 0; i < repository.length; i++) {
-//   document.write(repository[i].name + ' ' +  repository[i].types + '(height: ' + repository[i].height + ')' + ' ' + '<br>');
-//   if (repository[i].height >= 7) {
-//     document.write('- Wow, that\'s big ');
-//   };
-// };
+  function getAll () {
+    return repository;
+  }
 
-
-// currently writing only the keys in the object below.
-
-// IIFE testing
-var pokemonRepository = (function() {
-  var data = {};
-
-// currently printing out each line, can change each key to be seperate pokemon
-
-var repository =
-  {
-    name: ['Bulbasaur',' Charmander',' Squirtle'],
-    height: [7, 6, 5],
-    types: ['grass and poison', ' fire ', ' water ']
+  return {
+    add: add,
+    getAll: getAll
   };
 
-//creating printing without using the for loop
+})()
 
-  Object.keys(repository).forEach(function (pokemonData) {
-    document.write(pokemonData + ': ' + repository[pokemonData] + '<br>');
-  });
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'mewstwostheblues', height: '8', types: 'gangster' });
+console.log(pokemonRepository.getAll());
 
-  // end IIFE testing brackets
+// creating printing without using the for loop
 
-})();
-
-
-// trying for each function to print off each pokemon, but above is already an array. Duh it's using object.keys
-
-// prints all key names in the object
-
-//  var allProperties = Object.keys(repository);
-//
-// document.write(allProperties);
-
-// trying for each function to print off each pokemon, but above is already an array.
-
-// object.keys(repository).forEach(function(repKeys) {
-//   document.write(repository[repKeys]);
-// });
-
-// print each pokemon with a for loop
-
-
-
-// trying to mess around with colors?
-// if (repository[0, 1, 2].types = 'grass') {
-//   backgroundColor = '#99C262'
-// };
+pokemonRepository.getAll().forEach(function (pokemonData) {
+  document.write('name:' + pokemonData.name + 'height: ' + pokemonData.height + ' ' + 'types:' + pokemonData.types + '<br>');
+});
