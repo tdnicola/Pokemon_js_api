9ffbcd0d9107742bb809a5cc25a96469ad51ed8a
Tony Nicola <tdnicola@gmail.com>
2019-02-13 21:47:28 -0700

    Updated js to pull pokemon names and url from pokemon API. Added fetch and promise polyfiles to js folder.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,14 @@
 var pokemonRepository = (function () {
-  var repository = [
-    {
-      name: 'Bulbasaur ',
-      height: 7,
-      types: 'grass'
-    },
-    {
-      name: 'Charmander ',
-      height: 6,
-      types: 'fire '
-    },
-    {
-      name: 'Squirtle ',
-      height: 5,
-      types: 'water '
-    }
-  ]
-
+  var repository = []
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   // allows to add a new pokemon to the array repository
-  function add (repository) {
-    repository.push(repository);
+  function add (pokemon) {
+    repository.push(pokemon);
   }
 
   function getAll () {
     return repository;
   }
-
-  // returns the functions outside of the iife
-
-  return {
-    add: add,
-    getAll: getAll,
-    addListItem: addListItem
-  };
 
   // Function that creates li inside of ul elements and inserts the pokemon names
 
@@ -44,17 +20,65 @@ var pokemonRepository = (function () {
     $li.appendChild($button);
     $button.innerText = repository.name;
     $button.addEventListener('click', function (event) {
-      console.log(repository.name)
+      console.log(repository.name, repository.detailsUrl)
+    })
+    }
+
+
+  function loadList () {
+    return fetch(apiUrl).then(function (response){
+      return response.json ();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        var pokemon = {
+          name: item.name,
+          detailsUrl: item.url,
+       };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
     })
   }
-
-  function showDetails (pokemon) {
-    console.log(pokemon)
+  // Fetch data from API and add to the repository
+  function loadDetails(item) {
+    var url = item.detailsUrl;
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = Object.keys(details.types);
+    }).catch(function (e) {
+      console.error(e);
+    })
   }
+  // returns the functions outside of the iife
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
+  };
+
+
+
+
 })()
 
-// puts the elements onto the page from the addListItem above
 
-pokemonRepository.getAll().forEach(function (repository) {
-  pokemonRepository.addListItem(repository)
-})
+function showDetails (pokemon) {
+  pokemonRepository.loadDetails(pokemon).then(function() {
+    console.log(pokemon); })
+  }
+// puts the elements onto the page from the addListItem above
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon) });
+});
+
+
+// pokemonRepository.getAll().forEach(function (repository) {
+//  pokemonRepository.addListItem(repository)
+// })
